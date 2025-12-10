319df8bef79c6347b4e149a260f7800dffd9f66c
Kory <200974544+Kory221@users.noreply.github.com>
2025-12-10 18:43:34 +0100

    Remove Unecessary "if (!new.target)"
    Removed it from the constructor which is now inside the class.
    since a class can't be called without "new", it's unecessary.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,6 @@ const myLibrary = [];
 
 class Book {
     constructor (title, author, pages, status) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor")
-    };
     this.title = title;
     this.author = author;
     this.pages = pages;
